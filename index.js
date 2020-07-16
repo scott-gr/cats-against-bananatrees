634027c71f8c882190f5d11280a53e28616bd473
Mark Mooney <mjmoon15@gmail.com>
2020-07-16 10:24:59 -0500

    updated ports

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+var PORT = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
@@ -10,7 +12,7 @@ io.on('connection', (socket) => {
   console.log('a user connected');
 });
 
-http.listen(3000, () => {
+http.listen(PORT, () => {
   console.log('listening on *:3000');
 });
 
