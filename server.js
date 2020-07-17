22e1f6339e721bc5e6199db26df5027c52113088
Scott Griffin <mscottgriffin@gmail.com>
2020-07-17 12:24:42 -0400

    game.js pseudo started

--- server.js
+++ server.js
@@ -1,32 +1,43 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('/', function(req, res) {
-   res.sendfile('index.html');
+// Set Handlebars.
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+app.use(express.static('public'));
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
 });
-
+//Sets up username in array
 users = [];
-io.on('connection', function(socket) {
-   console.log('A user connected');
-   socket.on('setUsername', function(data) {
-      console.log(data);
-      
-      if(users.indexOf(data) > -1) {
-         socket.emit('userExists', data + ' username is taken! Try some other username.');
-      } else {
-         users.push(data);
-         socket.emit('userSet', {username: data});
-      }
-   });
-   
-   socket.on('msg', function(data) {
-      //Send message to everyone
-      io.sockets.emit('newmsg', data);
-   })
-});
+io.on('connection', function (socket) {
+  console.log('A user connected');
+  socket.on('setUsername', function (data) {
+    console.log(data);
+    console.log(socket.id);
+    //checks new username against existing array
+    if (users.indexOf(data) > -1) {
+      socket.emit(
+        'userExists',
+        data + ' You found the princess. BWAH! She is in another house.'
+      );
+    } else {
+      users.push(data);
+      socket.emit('userSet', { username: data });
+    }
+  });
 
-http.listen(PORT, function() {
-   console.log('listening on PORT: ' + PORT);
+  socket.on('msg', function (data) {
+    //Send message to everyone
+    io.sockets.emit('newmsg', data);
+  });
+});
+http.listen(PORT, function () {
+  console.log('listening on port: ' + PORT);
 });
