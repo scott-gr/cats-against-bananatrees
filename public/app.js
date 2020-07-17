24668fbfd6432ed579b2d8b2429ae0a6a255e6d5
Eliza Regas <elizaregas@gmail.com>
2020-07-17 19:16:21 -0400

    navigating to game pgae in place, and player not allowed to advance without entering a name

--- public/app.js
+++ public/app.js
@@ -1,35 +1,46 @@
+let socket = io();
 
+function setUsername() {
+  const nameInput = $("#name").val();
+  if (nameInput !== "") {
+    location.href = "/game";
+    socket.emit("setUsername", $("#game").val());
+  } 
+  else {
+    console.log("placeholder");
+    $("#name").css("background-color", "pink");
+  }
+}
 
-   let socket = io();
+const enableButton = (event) => {
+  console.log("called");
+  console.log(event);
+}
 
-   function setUsername() {
-     location.href = '/game';
-     socket.emit("setUsername", $("#game").val());
-
-   }
-   let user;
-   socket.on("userExists", function (data) {
-     $("#error-container").css("display", "block");
-     $("#error-container").html(data);
-   });
-   socket.on("userSet", function (data) {
-     user = data.username;
-     document.body.innerHTML =
-       '<input type = "text" id = "message">\
+let user;
+socket.on("userExists", function (data) {
+  $("#error-container").css("display", "block");
+  $("#error-container").html(data);
+});
+socket.on("userSet", function (data) {
+  user = data.username;
+  document.body.innerHTML =
+    '<input type = "text" id = "message">\
         <button type = "button" name = "button" onclick = "sendMessage()">Send</button>\
         <div id = "message-container"></div>';
-   });
-   function sendMessage(data) {
-    console.log("message for you, sir", data)
-     let msg = $("#message").val();
-     if (msg) {
-       socket.emit("msg", { message: msg, user: user });
-       
-     }
-   }
-   socket.on("newmsg", function (data) {
-     console.log("message", data);
-     if (user) {$("#message-container").html(
-       "<div><b>" + data.user + "</b>: " + data.message + "</div>") 
-     }
-   });
+});
+function sendMessage(data) {
+  console.log("message for you, sir", data);
+  let msg = $("#message").val();
+  if (msg) {
+    socket.emit("msg", { message: msg, user: user });
+  }
+}
+socket.on("newmsg", function (data) {
+  console.log("message", data);
+  if (user) {
+    $("#message-container").html(
+      "<div><b>" + data.user + "</b>: " + data.message + "</div>"
+    );
+  }
+});
