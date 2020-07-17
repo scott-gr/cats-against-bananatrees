fac5719a779bcf51e7978cbf943cb899421a4d91
Eliza Regas <elizaregas@gmail.com>
2020-07-17 13:44:22 -0400

    css working

--- server.js
+++ server.js
@@ -3,13 +3,11 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const PORT = process.env.PORT || 3000;
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Set Handlebars.
-var exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.use(express.static('public'));
