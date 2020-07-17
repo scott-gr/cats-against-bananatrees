8375123ce2066c9c42ed6d8f36539487a22c2b32
Eliza Regas <elizaregas@gmail.com>
2020-07-17 15:57:11 -0400

    logo in place

--- public/app.js
+++ public/app.js
@@ -1,14 +1,14 @@
 
 
    let socket = io();
+
    function setUsername() {
-     console.log("before", $("#name").val())
      socket.emit("setUsername", $("#name").val());
-     console.log("after")
+     
    }
    let user;
    socket.on("userExists", function (data) {
-     $("#error-container").innerHTML = data;
+     $("#error-container").html(data);
    });
    socket.on("userSet", function (data) {
      user = data.username;
@@ -17,15 +17,17 @@
         <button type = "button" name = "button" onclick = "sendMessage()">Send</button>\
         <div id = "message-container"></div>';
    });
-   function sendMessage() {
-     let msg = $("#message").value;
+   function sendMessage(data) {
+    console.log("message for you, sir", data)
+     let msg = $("#message").val();
      if (msg) {
        socket.emit("msg", { message: msg, user: user });
+       
      }
    }
    socket.on("newmsg", function (data) {
-     if (user) {$("#message-container").innerHTML +=
-         "<div><b>" + data.user + "</b>: " + data.message + "</div>";
+     console.log("message", data);
+     if (user) {$("#message-container").html(
+       "<div><b>" + data.user + "</b>: " + data.message + "</div>") 
      }
    });
-
--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const PORT = process.env.PORT || 3000;
 
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Set Handlebars.
@@ -15,28 +19,32 @@ app.use(express.static('public'));
 app.get('/', function (req, res) {
   res.render(__dirname + '/views/index.handlebars');
 });
+
+app.get('/game', function (req, res) {
+   res.render(__dirname + '/views/game.handlebars');
+ });
+
 //Sets up username in array
 users = [];
 io.on('connection', function (socket) {
-  console.log('A user connected');
   socket.on('setUsername', function (data) {
-    console.log(data);
-    console.log(socket.id);
+    console.log("Username: ", data, "Socket ID: ", socket.id);
     //checks new username against existing array
     if (users.indexOf(data) > -1) {
       socket.emit(
         'userExists',
-        data + ' You found the princess. BWAH! She is in another house.'
+        'Hello, ' + data
       );
     } else {
       users.push(data);
       socket.emit('userSet', { username: data });
     }
   });
-
+//sends message
   socket.on('msg', function (data) {
     //Send message to everyone
     io.sockets.emit('newmsg', data);
+    console.log(data)
   });
 });
 
