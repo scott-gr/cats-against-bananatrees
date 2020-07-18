10ee43ceffe4d9d88f4014a3ce1fae44b24055b1
Eliza Regas <elizaregas@gmail.com>
2020-07-18 15:05:00 -0400

    chat working on game page

--- public/app.js
+++ public/app.js
@@ -1,8 +1,9 @@
 let socket = io();
+let user;
 
 function setUsername() {
   const nameInput = $("#indexName").val();
-  sessionStorage.setItem("hostName", nameInput);
+  sessionStorage.setItem("userName", nameInput);
   if (nameInput !== "") {
     location.href = "/pregame";
     socket.emit("setUsername", $("#indexName").val());
@@ -11,19 +12,37 @@ function setUsername() {
   }
 }
 
-function getHostName() {
-  console.log(sessionStorage.getItem("hostName"));
+const getUserName = () => {
+  return sessionStorage.getItem("userName");
 }
 
 const whiteBackground = () => {
   $("#indexName").css("background-color", "white");
 };
 
-let user;
+const submitChat = () => {
+  const chatInput = $("#chatInput").val();
+  if (chatInput !== "") {
+    const userName = getUserName();
+    socket.emit('msg', {
+      message: chatInput,
+      user: userName
+    })
+  }
+}
+
+socket.on("newmsg", (data) => {
+  const { message, user } = data;
+  const chatEntry = $(`<li>${user}: ${message}</li>`);
+  $("#chatEntries").append(chatEntry);
+})
+
 socket.on("userExists", function (data) {
   $("#error-container").css("display", "block");
   $("#error-container").html(data);
+  console.log("error-container data: " + data);
 });
+
 socket.on("userSet", function (data) {
   user = data.username;
   document.body.innerHTML =
@@ -31,13 +50,13 @@ socket.on("userSet", function (data) {
         <button type = "button" name = "button" onclick = "sendMessage()">Send</button>\
         <div id = "message-container"></div>';
 });
-function sendMessage(data) {
-  console.log("message for you, sir", data);
-  let msg = $("#message").val();
-  if (msg) {
-    socket.emit("msg", { message: msg, user: user });
-  }
-}
+// function sendMessage(data) {
+//   console.log("message for you, sir", data);
+//   let msg = $("#message").val();
+//   if (msg) {
+//     socket.emit("msg", { message: msg, user: user });
+//   }
+// }
 socket.on("newmsg", function (data) {
   console.log("message", data);
   if (user) {
