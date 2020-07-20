446deab0b70f200b449d851ff2653e42b48dbc76
Eliza Regas <elizaregas@gmail.com>
2020-07-20 19:01:34 -0400

    get route for rooms

--- controllers/roomsController.js
+++ controllers/roomsController.js
@@ -1,26 +1,29 @@
 const db = require("../models");
 module.exports = function (router) {
   // GET request not currently needed, TBD
-  // router.get("/api/getallrooms", (req, res) => {
-  //   db.Rooms.findAll({
-  //     attributes: ["id"],
-  //   })
-  //     .then((result) => {
-  //       res.json({
-  //         error: false,
-  //         data: result,
-  //         message: "Successfully retrieved rooms",
-  //       });
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //       res.status(500).json({
-  //         error: true,
-  //         data: null,
-  //         message: "Unable to retrieve rooms.",
-  //       });
-  //     });
-  // });
+  router.get("/api/getroom/:roomid", (req, res) => {
+    const roomId = parseInt(req.params.roomid);
+    db.Rooms.findOne({
+      where: {
+        id: roomId,
+      },
+    })
+      .then((result) => {
+        res.json({
+          error: false,
+          data: result,
+          message: "Successfully retrieved room.",
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({
+          error: true,
+          data: null,
+          message: "Unable to retrieve room.",
+        });
+      });
+  });
 
   router.post("/api/createnewroom", (req, res) => {
     db.Rooms.create(req.body)
