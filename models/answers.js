9541b987bca4094dd492b8e6ee75e75de16ba458
Scott Griffin <mscottgriffin@gmail.com>
2020-07-20 03:26:13 -0400

    deleted 2 test files

---
+++ models/answers.js
@@ -0,0 +1,15 @@
+// Create table for answer cards in database.
+
+// Seed in data for answer cards.
+
+// Create an answer cards model that contains a function to get all answer cards.
+
+// Create function in model to get one card by card id.
+
+// Create function in model to insert drawn card into player answer cards.
+
+// Create function in model to select a random number and check against player answer cards 
+
+// and round answer cards to ensure card id hasn't been drawn already. 
+// If so, select new random number. If not, call function to get card by id 
+// and add to player answer cards.
