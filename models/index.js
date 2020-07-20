67694d8e3f260d79de6e11ab7c6169d462838bb0
Eliza Regas <elizaregas@gmail.com>
2020-07-20 16:41:35 -0400

    heroku

--- models/index.js
+++ models/index.js
@@ -8,19 +8,11 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-// if (process.env.JAWSDB_WHITE_URL) {
-//   connection = mysql.createConnection(process.env.JAWSDB_WHITE_URL);
-// } else {
-//   connection = mysql.createConnection({
-//     host: 'localhost',
-//     port: 3306,
-//     user: 'root',
-//     password: 'Wino4ever',
-//     database: 'burgers_db'
-//   });
-// }
 let sequelize;
 if (config.use_env_variable) {
+  console.log("*******************env", env);
+  console.log("*******************configenv", config.use_env_variable);
+
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
